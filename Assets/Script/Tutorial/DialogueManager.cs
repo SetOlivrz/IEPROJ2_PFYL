2d7f2e1aadb6832e203b9ab4c2be09e73c15b1c4
Setiel B. Olivarez <setiel_b_olivarez@dlsu.edu.ph>
2022-02-11 09:41:56 +0800

    WIP tutorial scene

--- Assets/Script/Tutorial/DialogueManager.cs
+++ Assets/Script/Tutorial/DialogueManager.cs
@@ -7,60 +7,159 @@ using UnityEngine.UI;
 public class DialogueManager : MonoBehaviour
 {
 
-    //[SerializeField] GameObject panel;
-    //[SerializeField] GameObject popUp;
-    [SerializeField] Text dialogueText;
-    [SerializeField] Text nameText;
+    [SerializeField] public GameObject panel;
+    [SerializeField] public GameObject popUp;
+
+    [SerializeField] public Button nextButton;
+
+    [SerializeField] public Text dialogueText;
+    [SerializeField] public Text nameText;
+
+    public int nTutorialIndex = 0;
+
+    public float ticks = 0;
+    public float maxTicks = 0;
+
+    public bool startDelay = false;
 
 
-    private Queue<string> sentences;
+    [SerializeField] TutorialActionManager TAmanager;
 
     // Start is called before the first frame update
     void Start()
     {
-        sentences = new Queue<string>();
+        nTutorialIndex = 0;
+        panel.SetActive(false);
+        popUp.SetActive(false);
 
-        // panel fades in
-        // popup pops out
+        nextButton.interactable = false;
 
-        //panel.SetActive(true);
-        //popUp.SetActive(true);
-
+        //StartCoroutine(IntroTutorial());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    
-    public void StartDialogue(Dialogue dialogue)
-    {
-        Debug.Log("starting dialogue");
-        nameText.text = dialogue.name;
-        sentences.Clear();
-
-        foreach (string sentence in dialogue.sentences)
+        if (startDelay == true)
         {
-            sentences.Enqueue(sentence);
-            DisplayNextSentence();
+            ticks += Time.unscaledDeltaTime;
+
+            if (ticks >= maxTicks) // enables the button after the specified sec delay
+            {
+                EnableButton();
+                startDelay = false;
+                ticks = 0;
+            }
+        }
+        else if (ticks != 0 && startDelay ==false)
+        {
+            ticks = 0;
         }
     }
 
-    public void DisplayNextSentence()
+    IEnumerator IntroTutorial()
     {
-        if (sentences.Count == 0)
-        {
-            EndDialogue();
-            return;
-        }
+        // wait for 3 seconds
+        yield return new WaitForSeconds(3);
 
-        string sentence = sentences.Dequeue();
-        dialogueText.text = sentence;
+        // display the first popup
+        panel.SetActive(true);
+        popUp.SetActive(true);
+
+        PauseGame();
+        DisableButton();
+
+        maxTicks = 5.0f;
+        startDelay = true;
+        // wait for 5 seconds
+
+        // make next button interactable
     }
 
-    public void EndDialogue()
+    // call this function to proceed to the tutorial
+    // case 1: when the player selects the next button
+    // case 2: when the player satisfies the condition to trigger the next step in the tutorial
+    public void ProceedTutorial()
     {
-        Debug.Log("End Conversation");
+        nTutorialIndex++;
+        // pause
+        PauseGame();
+        DisableButton();
+
+        if (nTutorialIndex == 1) // instruction for movemnet
+        {
+            dialogueText.text = "Press WASD to move around";
+            startDelay = true;
+            maxTicks = 5;
+
+        }
+        else if (nTutorialIndex ==2) // execution for player movement
+        {
+            popUp.SetActive(false);
+            panel.SetActive(false);
+            // resume
+            ResumeGame();
+
+            // set the item to pick up to active (from TAmanager script)
+            TAmanager.parentContainer.SetActive(true);
+
+            // step 2, has to be checked in da TA manager
+            
+        }
+        else if (nTutorialIndex == 3)// intro to planting after movement tutorial
+        {
+
+            popUp.SetActive(true);
+            panel.SetActive(true);
+            PauseGame();
+            DisableButton();
+            dialogueText.text = " Now, let’s try planting ";
+
+            startDelay = true;
+            maxTicks = 5;
+        }
+        else if (nTutorialIndex ==4)// instruction on how to use a hoe
+        {
+            dialogueText.text = " Pressing num keys 1-6 to allows you to change the equipped tool or use item from you inventory.";
+            startDelay = true;
+            maxTicks = 3;
+
+        }
+        else if (nTutorialIndex == 5)
+        {
+            dialogueText.text = " For easier access, you may also perform this using the scroll wheel.";
+            startDelay = true;
+            maxTicks = 5;
+        }
+        // disables button when next if pressed
+
+    }
+
+    public void  EnableButton()
+    {
+        nextButton.interactable = true;
+    }
+
+    public void DisableButton()
+    {
+        nextButton.interactable = false;
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+
+    IEnumerator SecDelayBeforeNext(float sec)
+    {
+        yield return new WaitForSeconds(sec);
+        EnableButton();
     }
 }
+    
+   
